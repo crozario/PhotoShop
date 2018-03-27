ca93a058065a1a7bf9f8c6204803ffe6b8d89399
Crossley Rozario <crozariodev@gmail.com>
2018-03-27 12:38:07 -0400

    added project pdfs

--- src/database/database.py
+++ src/database/database.py
@@ -1,16 +1,16 @@
-class PhotoShopDatabase:
-	def __init__(self, db):
-		self.db = db
+# class PhotoShopDatabase:
+# 	def __init__(self, db):
+# 		self.db = db
 
-	def createTables():
-		createPhotoTable()
-		createLandScapeTable()
-		createLocationTable()
-		createAbstractTable()
-		createModelsTable()
-		createModelTable()
-		createPhotographerTable()
-		createInfuencesTable()
-		createTransactionTable()
-		createCustomerTable()
+# 	def createTables():
+# 		createPhotoTable()
+# 		createLandScapeTable()
+# 		createLocationTable()
+# 		createAbstractTable()
+# 		createModelsTable()
+# 		createModelTable()
+# 		createPhotographerTable()
+# 		createInfuencesTable()
+# 		createTransactionTable()
+# 		createCustomerTable()
 

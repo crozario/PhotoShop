f12cce9b9089903f0668336914feceaa6121e00f
Crossley Rozario <crozariodev@gmail.com>
2018-04-17 13:09:15 -0400

    created all tables for photoshop

--- src/database/photoshopdb.py
+++ src/database/photoshopdb.py
@@ -3,6 +3,10 @@ import sys
 
 def setupPhotoShopDatabase(cursor):
 	createTables(cursor)
+	print("Created Tables")
+
+	createConstraints(cursor)
+	print("Created Constraints")
 
 def createTables(cursor):
 	createPhotoTable(cursor)
@@ -18,44 +22,149 @@ def createTables(cursor):
 
 
 def createPhotoTable(cursor):
-	query = """CREATE TABLE IF NOT EXIST Photo( )
+	query = """CREATE TABLE IF NOT EXIST `Photo` (
+		`PhotoID` INT UNSIGNED AUTO_INCREMENT,
+  		`Speed` VARCHAR(45) NULL,
+  		`Film` VARCHAR(45) NULL,
+  		`F-Stop` VARCHAR(12) NULL,
+  		`Color/B&W` ENUM('Color', 'B&W') NULL,
+		`Resolution` VARCHAR(25) NULL,
+  		`Price` FLOAT UNSIGNED,
+  		`Date` DATE NULL,
+  		`TransID` INT UNSIGNED NULL,
+		`PName` VARCHAR(25) NULL,
+		`PBDate` DATE NULL,
+  		PRIMARY KEY (`PhotoID`),
+	)
+	ENGINE=InnoDB;
 	"""
 	cursor.execute(query)
-	response = cursor.fetchone()
-	
-	return response 
 
 def createLandScapeTable(cursor):
-	pass
+	query = """CREATE TABLE IF NOT EXIST `Landscape` (
+		`PhotoID` INT UNSIGNED,
+		`Place` VARCHAR(45) NULL,
+		`Country` VARCHAR(45) NULL,
+		PRIMARY KEY (`PhotoID`)
 
+	)
+	ENGINE=InnoDB;
+	"""
+	cursor.execute(query)
 
 def createLocationTable(cursor):
-	pass
+	query = """CREATE TABLE IF NOT EXIST `Location` (
+		`Place` VARCHAR(45),
+  		`Country` VARCHAR(45),
+  		`Description` TEXT NULL,
+  		PRIMARY KEY (`Place`, `Country`)
+	)
+	ENGINE=InnoDB;
+	"""
+	cursor.execute(query)
 
 def createAbstractTable(cursor):
-	pass
+	query = """CREATE TABLE IF NOT EXIST `Abstract` (
+		`PhotoID` INT UNSIGNED,
+		`Comment` Text NULL,
+		PRIMARY KEY (PhotoID)
+	)
+	ENGINE=InnoDB;
+	"""
+	cursor.execute(query)
 
 def createModelsTable(cursor):
-	pass
+	query = """CREATE TABLE IF NOT EXIST `Models` (
+		`PhotoID` INT UNSIGNED,
+		`MName` VARCHAR(25),
+		`MBDate` DATE,
+		`Agency` VARCHAR(45) NULL,
+		PRIMARY KEY (`PhotoID`, `MName`, `MBDate`)
+	)
+	ENGINE=InnoDB;
+	"""
+
+	cursor.execute(query)
 
 def createModelTable(cursor):
-	pass
+	query = """CREATE TABLE IF NOT EXIST `Model` (
+		`MName` VARCHAR(25) NOT NULL,
+  		`MBDate` DATE NOT NULL,
+		`MBio` TEXT NULL,
+  		`MSex` CHAR(1) NULL,
+  		PRIMARY KEY (`MName`, `MBDate`)
+	)
+	ENGINE=InnoDB;
+	"""
+	cursor.execute(query)
 
 def createPhotographerTable(cursor):
-	pass
+	query = """CREATE TABLE IF NOT EXIST `Photographer` (
+		`PName` VARCHAR(25),
+  		`PBDate` DATE,
+  		`PBio` TEXT NULL,
+ 		`PBAddress` TEXT NULL,
+ 		`PNationality` VARCHAR(45) NULL,
+ 		 PRIMARY KEY (`PName`, `PBDate`)
+	)
+	ENGINE=InnoDB;
+	"""
+	cursor.execute(query)
 
 def createInfuencesTable(cursor):
-	pass
+	query = """CREATE TABLE IF NOT EXIST `Influences` (
+		`EPName` VARCHAR(25),
+  		`EPBDate` DATE,
+		`RPName` VARCHAR(25),
+  		`RPBDate` DATE,
+  		PRIMARY KEY (`EPName`, `EPBDate`, `RPName`, `RPBDate`))
+	)
+	ENGINE=InnoDB;
+	"""
+	cursor.execute(query)
 
 def createTransactionTable(cursor):
-	pass
+	query = """CREATE TABLE IF NOT EXIST `Transaction` (
+		`TransID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  		`TDate` DATE NOT NULL,
+  		`CardNo` INT UNSIGNED NOT NULL,
+ 		`CardType` VARCHAR(25) NOT NULL,
+  		`CardExpDate` DATE NOT NULL,
+  		`TotalAmount` INT UNSIGNED NOT NULL,
+  		`LoginName` VARCHAR(25) NOT NULL,
+  		PRIMARY KEY (`TransID`)
+	)
+	ENGINE=InnoDB;
+	"""
+	cursor.execute(query)
 
 def createCustomerTable(cursor):
-	pass
+	query = """CREATE TABLE IF NOT EXIST `Customer` (
+		`LoginName` VARCHAR(25) NOT NULL,
+  		`Password` VARCHAR(25) NOT NULL,
+  		`CName` VARCHAR(25) NOT NULL,
+  		`CType` VARCHAR(45) NULL,
+  		`BillingAddress` TEXT NOT NULL,
+  		`Str1` VARCHAR(45) NULL,
+  		`Str2` VARCHAR(45) NULL,
+  		`City` VARCHAR(45) NULL,
+  		`State` CHAR(2) NULL,
+  		`Zip` VARCHAR(12) NULL,
+  		PRIMARY KEY (`LoginName`)
+	)
+	ENGINE=InnoDB;
+	"""
+	cursor.execute(query)
 
 
+def createConstraints(cursor):
+	query = """
 
 
+	)
+	ENGINE=InnoDB;
+	"""
+	cursor.execute(query)
 
 
 
